caef5346a72031ac9035f3a982e9957de7019e96
rcarmas <rcarmas@espe.edu.ec>
2023-05-23 15:44:31 -0500

    Update IV

--- Portafolio/js/app.js
+++ Portafolio/js/app.js
@@ -17,14 +17,29 @@ Vue.component('portfolio-item', {
 
   new Vue({
     el: '#app',
-    // data: {
-    //   portfolioItems: [
-    //     {
-    //       name: 'Dilan Zurita',
-    //       description: 'Joven estudiante universitario de sexto semestre en la carrera de tecnologías de la información.',
-    //       image: 'assets/img/dilan.JPG'
-    //     },
-    //     // Agrega más elementos del portafolio aquí si es necesario
-    //   ]
-    // }
+     data: {
+       portfolioItems: [
+        {
+            name: 'Christopher Armas',
+            description: 'Estudiante de Ingeniería en Tecnologías de la Información, actualmente estoy cursando sexto semestre de la carrera en la Universadad de las Fuerzas Armadas ESPE. Tengo conocimientos previos en Desarollo Web, Gestión de Base de Datos, Infraestructura, Soporte, Cyberseguridad e IA. Actualmente ejerzo como Desarollador Freelancer Jr. para la creación de diversas páginas y aplicaciones web - moviles con experiencia previa de las mismas en el ámbito del Frond End.',
+            image: 'assets/img/chris.JPG'
+          },
+        {
+            name: 'Sebastián Cárdenas',
+            description: 'Soy un estudiante de ingeniería en TI cursando sexto semestre, con enfoque en el desarrollo web. Actualmente trabajo en Bou Company, en el equipo de desarrolladores y operaciones, donde he desarrollado habilidades en solución de problemas y trabajo en equipo. Busco una posición donde pueda continuar aprendiendo y creciendo profesionalmente en el campo de la ingeniería en TI.',
+            image: 'assets/img/sebas.JPG'
+          },
+        {
+            name: 'Rafael Fonseca',
+            description: 'Estudiante de Ingeniería en Tecnologías de la Información. Me considero una persona responsable, proactiva, organizada, capaz que ayuda a los demás en caso que lo necesiten.',
+            image: 'assets/img/rafa.JPG'
+          },
+         {
+           name: 'Dilan Zurita',
+           description: 'Joven estudiante universitario de sexto semestre en la carrera de tecnologías de la información, amable, extrovertido, responsable y polifacético. Poseo habilidades computacionales diversas las cuales abarcan las necesidades básicas de las ramas de la tecnología como: la gestion de base de datos desarrollo front y back end manejo de distintos lenguajes de programación redes computacionales Habilidades que se adaptarán dependiendo de las necesidades que se presenten en mi lugar laboral.',
+           image: 'assets/img/dilan.JPG'
+         },
+         
+       ]
+    }
   });
